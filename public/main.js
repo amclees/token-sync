73b6d0a8f9ba84289ee2fad620a7bebfcab3359c
amclees <andrewmclees5@gmail.com>
2017-08-24 18:38:47 -0700

    Restructure database to allow for multiple rooms

--- public/main.js
+++ public/main.js
@@ -7,25 +7,8 @@
     [
       '$scope', '$firebase', '$firebaseAuth', '$firebaseObject',
       function($scope, $firebase, $firebaseAuth, $firebaseObject) {
-        var roomId = null;
-        try {
-          var getParameters = window.location.search.split('&')[1].split('='),
-              roomNext = false;
-          for (var i = 0; i < getParameters.length; i++) {
-            if (i % 2 === 0 && getParameters[i] === 'room') {
-              roomId = getParameters[i + 1];
-              break;
-            }
-          }
-        } catch (error) {
-          // Room parameter not included
-        }
-
-        if (roomId) {
-          $scope.joinRoomById(room);
-        }
-
         $scope.roomsRef = firebase.database().ref('rooms');
+        $scope.roomOwnersRef = firebase.database().ref('roomOwners');
         $scope.authObj = $firebaseAuth();
         $scope.actionInProgress = false;
 
@@ -36,7 +19,7 @@
           }
           $scope.authObj.$signInWithPopup(provider).then(function(authData) {
             $scope.authData = authData;
-            $scope.joinUserRoomIfExists();
+            $scope.getExistingRooms();
           }, function(error) {
              console.error('Login failed: ', error);
           });
@@ -48,32 +31,59 @@
             $scope.authData = null;
             $scope.room = null;
             $scope.actionInProgress = false;
+            $scope.$apply();
           });
         };
 
-        $scope.joinUserRoomIfExists = function() {
+        $scope.getExistingRooms = function() {
           $scope.actionInProgress = true;
-          $scope.roomsRef.child($scope.authData.user.uid).once('value', function(snapshot) {
-            if (snapshot.val() !== null) {
-              $scope.roomRef = $scope.roomsRef.child($scope.authData.user.uid);
-              $scope.room = $firebaseObject($scope.roomRef);
+          $scope.userRoomsRef = $scope.roomsRef.child($scope.authData.user.uid);
+          $scope.existingRooms = $firebaseObject($scope.userRoomsRef);
+          $scope.actionInProgress = false;
+        };
+
+        $scope.joinRoomIfExists = function(roomId, callback) {
+          var joined = false;
+          $scope.actionInProgress = true;
+
+          $scope.roomOwnersRef.child(roomId).once('value', function(owner) {
+            if (owner.val() === null) {
+              if (callback) {
+                callback(false);
+              }
+              return;
             }
-            $scope.actionInProgress = false;
+
+            $scope.roomsRef.child(owner.val().id).once('value', function(snapshot) {
+              if (snapshot.val() !== null) {
+                $scope.roomRef = $scope.roomsRef.child(owner.val().id).child(roomId);
+                $scope.room = $firebaseObject($scope.roomRef);
+                joined = true;
+              }
+              $scope.actionInProgress = false;
+              $scope.$apply();
+              if (callback) {
+                callback(joined);
+              }
+            });
           });
         };
 
-
-        $scope.joinRoomByInvite = function(invite) {
-
+        $scope.showInviteCode = function() {
+          $scope.inviteCode = $scope.room.id;
         };
 
         $scope.createRoom = function() {
           $scope.actionInProgress = true;
-          $scope.roomRef = $scope.roomsRef.child($scope.authData.user.uid);
+          $scope.roomRef = $scope.roomsRef.child($scope.authData.user.uid).push();
           $scope.room = $firebaseObject($scope.roomRef);
+          $scope.room.id = $scope.roomRef.key;
           $scope.room.name = $scope.roomName;
           $scope.room.tokenName = $scope.tokenName;
           $scope.room.$save();
+          $scope.roomOwnersRef.child($scope.room.id).set({
+            'id': $scope.authData.user.uid
+          });
           $scope.actionInProgress = false;
         };
       }
